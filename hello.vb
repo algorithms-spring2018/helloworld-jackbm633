15ce4ab1c896ab644f80110e4ebf6d2708f748d5
Jack Beckitt-Marshall <jack@pictura.co.uk>
2018-02-20 15:15:06 -0500

    Updated VB version

--- hello.vb
+++ hello.vb
@@ -2,5 +2,7 @@ Module Module1
 
     Sub Main
         Console.WriteLine("Hello, World")
+        Console.WriteLine("This is coded in VB.Net")
+        Console.WriteLine("^_^")
     End Sub
 End Module
